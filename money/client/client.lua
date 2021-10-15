8e2428716278a7566a835bd7d1b1ae8e88645deb
xDreamLand <45896839+xDreamLand@users.noreply.github.com>
2021-10-15 23:57:31 +0200

    Update client.lua
    Consider taking a look at this: https://github.com/project-error/fivem-lua-style. Not necessary, but it's good practice!

--- money/client/client.lua
+++ money/client/client.lua
@@ -1,13 +1,12 @@
 local stuff = {}
-Citizen.CreateThread(function() 
+CreateThread(function() 
     if NetworkIsSessionActive() then
-        Citizen.Wait(100)
+        Wait(100)
         TriggerServerEvent('NAT2K15:UPDATEMONEY')
     end
 end)
 
-RegisterNetEvent('NAT2K15:UPDATECLIENTMONEY')
-AddEventHandler('NAT2K15:UPDATECLIENTMONEY', function(coolarray)
+RegisterNetEvent('NAT2K15:UPDATECLIENTMONEY', function(coolarray)
     local id = GetPlayerServerId(PlayerId())
     stuff = coolarray
     if(stuff[id]) then
@@ -15,8 +14,7 @@ AddEventHandler('NAT2K15:UPDATECLIENTMONEY', function(coolarray)
     end
 end)
 
-RegisterNetEvent('NAT2K15:UPDATEPAYCHECK')
-AddEventHandler('NAT2K15:UPDATEPAYCHECK', function(moneyarray) 
+RegisterNetEvent('NAT2K15:UPDATEPAYCHECK', function(moneyarray) 
     stuff = moneyarray
     local id = GetPlayerServerId(PlayerId())
     if(stuff[id]) then
@@ -28,14 +26,12 @@ AddEventHandler('NAT2K15:UPDATEPAYCHECK', function(moneyarray)
     end
 end)
 
-RegisterNetEvent('NAT2K15:UPDATEPAY')
-AddEventHandler('NAT2K15:UPDATEPAY', function(id, money) 
+RegisterNetEvent('NAT2K15:UPDATEPAY', function(id, money) 
     stuff = money
     SendNUIMessage({action = "update_money", amount = stuff[id].amount, bank = stuff[id].bank})
 end)
 
-RegisterNetEvent('NAT2K15:BANKNOTIFY')
-AddEventHandler('NAT2K15:BANKNOTIFY', function(msg) 
+RegisterNetEvent('NAT2K15:BANKNOTIFY', function(msg) 
     BeginTextCommandThefeedPost("STRING")
     AddTextComponentSubstringPlayerName(msg)
     EndTextCommandThefeedPostMessagetext("CHAR_BANK_FLEECA", "CHAR_BANK_FLEECA", false, 9, "Bank", "Account Notification")
